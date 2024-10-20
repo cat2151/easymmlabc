38491955e944263f6483d0aa4b2e8704b5658425
cat2151 <fct_2004@mail.goo.ne.jp>
2024-10-20 16:39:32 +0900

    SHIFT+ENTER（現在位置を演奏）と、CTRL+S（先頭から演奏）を実装した

--- easymmlabc.js
+++ easymmlabc.js
@@ -10,6 +10,7 @@ easymml.init = function() {
   easyabcjs6.init();
 
   setupSelect(easymml);
+  setupKeyBind(easymml);
 
   easyabcjs6.play = easymml.play;
   easyabcjs6.init();
@@ -34,10 +35,63 @@ easymml.init = function() {
       }
     }
   }
+
+  function setupKeyBind(easymml) {
+    easymml.mml.addEventListener('keypress', function(e) {
+      if (e.keyCode === 13 && e.shiftKey) { // SHIFT+ENTER
+        e.preventDefault(); // 改行しない
+        easymml.playWithTimeShifter();
+      }
+    });
+    easymml.mml.addEventListener('keydown', function(e) {
+      if (e.keyCode === 83 && e.ctrlKey) { // CTRL+S
+        e.preventDefault(); // ページ保存ダイアログを開かない
+        easymml.play();
+      }
+    });
+  }
 }
 
 easymml.play = function() {
   const mml = easymml.mml.value;
+  easymml.playSub(mml);
+}
+
+easymml.playWithTimeShifter = function() {
+  const orgMml = easymml.mml.value;
+  const mml = easymml.insertTimeShifter(orgMml, easymml.mml.selectionStart);
+  easymml.playSub(mml);
+}
+
+easymml.onChangeSelect = function() {
+  const options = document.querySelectorAll(easymml.mmlTemplateId + " option");
+  easymml.mml.value = options[easymml.select.selectedIndex].value;
+  easymml.play();
+}
+
+easymml.insertTimeShifter = function(mml, pos) {
+  pos = getLastNotePos(mml, pos);
+  if (!pos) return mml;
+  mml = mml.slice(0, pos) + "!!" + mml.slice(pos);
+  return mml;
+
+  function getLastNotePos(mml, pos) {
+    let noteOrTrackSeparator = findLastMatchPos(mml, pos, /[a-g;]/); // ; は複数track用
+    if (noteOrTrackSeparator && mml[noteOrTrackSeparator] == ";") return noteOrTrackSeparator + 1; // 前のtrackのnoteではなく、今のtrack先頭を示す用
+    return findLastMatchPos(mml, pos, /[a-g]/);
+
+    function findLastMatchPos(mml, pos, regex) {
+      for (let i = pos; i >= 0; i--) {
+        if (mml[i] && regex.test(mml[i])) { // mml[i] がfalsyかを見るのは文字列末尾用
+          return i;
+        }
+      }
+      return 0;
+    }
+  }
+}
+
+easymml.playSub = function(mml) {
   let abc = "";
 
   try {
@@ -48,12 +102,6 @@ easymml.play = function() {
 
   easyabcjs6.abcNotation.value = abc;
   easyabcjs6.playSub(abc, ABCJS, easyabcjs6.musicScoreId);
-}
-
-easymml.onChangeSelect = function() {
-  const options = document.querySelectorAll(easymml.mmlTemplateId + " option");
-  easymml.mml.value = options[easymml.select.selectedIndex].value;
-  easymml.play();
 }
 
 easymml.init();
